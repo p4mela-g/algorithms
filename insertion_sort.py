c2a84f49999238dd06ba1d66cf4dddb8d368b195
Pâmela Gudeliauskas <47401012+gudeliauskaspam@users.noreply.github.com>
2020-02-09 22:57:20 -0300

    Algoritmos: Teoria e Prática
    por Thomas Cormen

---
+++ insertion_sort.py
@@ -0,0 +1,20 @@
+# INSERTION-SORT
+import numpy as np
+
+def insertion_sort(limit, x):
+    a = np.random.randint(limit , size = x)
+
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and key < a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+    return a
+
+question1 = int(input('How many numbers you want to order?'))
+question2 = int(input('And, now, what is the limit number?'))
+
+results = insertion_sort(question2, question1)
+print(*results, sep=' -> ')
